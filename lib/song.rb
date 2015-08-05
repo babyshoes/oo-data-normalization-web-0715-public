2dc3b7cd45504599906e2ae584474d503b9b80b2
babyshoes <dianecai@gmail.com>
2015-08-05 13:26:31 -0400

    all done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Song
-  # code goes here
+  
+  attr_accessor :artist, :title
+
+  # def title=(title)
+  #   @title = title
+  # end
+
+  # def artist=(artist)
+  #   @artist = artist
+  # end
+
+  def serialize
+    sanitized_title = (@title.split.map &:downcase).join("_")
+    open("./tmp/#{sanitized_title}.txt", 'w') {|f| f << "#{@artist.name} - #{@title}"}
+  end
+
 end
